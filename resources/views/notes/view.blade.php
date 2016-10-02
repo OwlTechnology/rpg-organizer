05f52e8775b8dbb870389832e6637d1a8643c607
Ivan Mattie <mattieivan@gmail.com>
2016-10-02 16:49:40 -0600

    Updated the note view stylings

--- resources/views/notes/view.blade.php
+++ resources/views/notes/view.blade.php
@@ -12,10 +12,15 @@
 <div class="content">
     <div class="content-body note">
 
-        <div class="breadcrumbs">
-            <a class="breadcrumb" href="{{ url("/campaign/" . $campaign->id . "/notes/") }}">
-                Back to Campaign
-            </a>
+        <div class="breadcrumbs spaced">
+            /
+            <a class="breadcrumb" href="{{ url("/campaign/" . $campaign->id . "/") }}">{{ $campaign->name }}</a>
+            /
+            <a class="breadcrumb" href="{{ url("/campaign/" . $campaign->id . "/notes/") }}">Notes</a>
+            /
+            <span class="breadcrumb">
+                {{ $note->name }}
+            </span>
         </div>
 
         <h1 class="name">{{ $note->name }}</h1>
@@ -24,11 +29,11 @@
             {{$note->content}}
         </p>
         <div class="actions">
-            <a href="{{ url('/campaign/' . $campaign->id . '/notes/' . $note->id . '/edit/') }}">
-                Edit Note
+            <a class="icon-button blue" href="{{ url('/campaign/' . $campaign->id . '/notes/' . $note->id . '/edit/') }}">
+                <i class="material-icons">edit</i><span class="text">Edit Note</span>
             </a>
-            <button onclick="toggleDeleteModal(true)">
-                Delete Note
+            <button class="icon-button red" onclick="toggleDeleteModal(true)">
+                <i class="material-icons">delete</i><span class="text">Delete Note</span>
             </button>
         </div>
     </div>
@@ -36,15 +41,17 @@
 
 <div class="modal-wrapper" id="delete-modal">
     <div class="modal">
-        <p>
-            Are you sure you want to delete {{$note->name}}?
-        </p>
+        <div class="content">
+            <p>
+                Are you sure you want to delete {{$note->name}}?
+            </p>
+        </div>
         <div class="actions">
-            <a class="button" href="{{ url("/campaign/" . $campaign->id . "/note/" . $note->id . "/delete/") }}">
-                Yes
+            <a class="button red" href="{{ url("/campaign/" . $campaign->id . "/note/" . $note->id . "/delete/") }}">
+                Delete
             </a>
             <button class="button" onclick="toggleDeleteModal(false)">
-                No
+                Cancel
             </button>
         </div>
     </div>
